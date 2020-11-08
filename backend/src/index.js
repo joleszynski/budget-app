a63c7d7ce717daadc25b91f2586ea013cba44650
Johny <jan.oleszynski@gmail.com>
2020-11-08 23:00:20 +0100

    Backend commit

---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config({ path: 'src/.env' });
+const cors = require('cors');
+
+const app = express();
+
+//dotenv.config();
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true }, () =>
+  console.log('Connected to db!'),
+);
+
+const authRoute = require('./routes/auth');
+const postRoute = require('./routes/posts');
+
+//Middleware
+app.use(express.json());
+app.use(cors());
+//RouteMiddleware
+app.use('/api/user', authRoute);
+app.use('/api/posts', postRoute);
+
+app.listen(3030, () => console.log('Server is running...'));
