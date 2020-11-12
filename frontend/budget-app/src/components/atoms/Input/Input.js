8ab1622d7861b70290d808f47beeccf5676ee41b
Johny <jan.oleszynski@gmail.com>
2020-11-12 23:03:19 +0100

    Mod styles

--- frontend/budget-app/src/components/atoms/Input/Input.js
+++ frontend/budget-app/src/components/atoms/Input/Input.js
@@ -3,9 +3,13 @@ import styled from 'styled-components';
 const Input = styled.input`
   width: 500px;
   height: 70px;
-  font-size: 37px;
+  font-size: 34px;
   font-family: ${({ theme }) => theme.mainFonts};
   padding-left: 30px;
+
+  &:focus-visible {
+    font-family: 'Montserrat';
+  }
 `;
 
 export default Input;
