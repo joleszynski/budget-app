05f3953bd6861efd7c418efa22710f48b228df05
Johny <jan.oleszynski@gmail.com>
2021-02-08 21:53:29 +0100

    Restyled atoms/ButtonIcon.js and delete comments

--- frontend/budget-app/src/components/atoms/ButtonIcon/ButtonIcon.js
+++ frontend/budget-app/src/components/atoms/ButtonIcon/ButtonIcon.js
@@ -50,19 +50,3 @@ export const ButtonIconPlus = () => (
     <VerticalLine />
   </Border>
 );
-
-// const ButtonIcon = styled.button`
-//   border: 2px solid ${({ theme }) => theme.mainColor};
-//   cursor: pointer;
-//   width: 25px;
-//   height: 25px;
-//   background-color: ${({ theme }) => theme.whiteColor};
-//   background-image: url(${({ iconBlack }) => iconBlack});
-
-//   &:hover {
-//     background-color: #000;
-//     background-image: url(${({ iconWhite }) => iconWhite});
-//   }
-// `;
-
-// export default ButtonIcon;
